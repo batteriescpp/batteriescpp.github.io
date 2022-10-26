c1998026544346461229923a4640cc29f1165174
Anthony Astolfi <tastolfi@gmail.com>
2022-10-26 14:58:56 -0400

    Test releaseNav.js.

---
+++ releaseNav.js
@@ -0,0 +1,5 @@
+document.onload(function () { if ($("#releaseNavSelectBox")) { return; } var navBarElem = $(".md-header nav");
+navBarElem.append("<select id=\"releaseNavSelectBox\" name=\"releaseNav\" onchange=\"location = this.value;\">"
+ + "<option value=\"/latest/\">latest</option>"
++ " <option value=\"/v0.12.1/\">v0.12.1</option>"
++ "</select>");});
