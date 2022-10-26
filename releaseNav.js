582a7969d5419c5612e27f4235cdfa66854fc396
Anthony Astolfi <tastolfi@gmail.com>
2022-10-26 15:35:41 -0400

    wip

--- releaseNav.js
+++ releaseNav.js
@@ -9,7 +9,7 @@ window.addEventListener('load', function() {
         ' <option value="/v0.12.1">v0.12.1</option>' +
         '</select>');
 
-    var actualVer = location.pathname.match(/^\([^\/]*/)[1];
+    var actualVer = location.pathname.match(/^\/[^\/]*/)[1];
     $('#releaseNavSelectBox').val(actualVer).change();
     $('#releaseNavSelectBox').change(function() {
         location.pathname.replace(/^\/[^\/]*/, this.value);
