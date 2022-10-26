eb772bd19c94ab4f84b1a2ebef07975ce130b07f
Anthony Astolfi <tastolfi@gmail.com>
2022-10-26 15:18:54 -0400

    Fix javascript bug.

--- releaseNav.js
+++ releaseNav.js
@@ -1,11 +1,11 @@
-window.addEventListener("load", function () {
-    if ($("#releaseNavSelectBox")) {
+window.addEventListener('load', function() {
+    if ($('#releaseNavSelectBox').length !== 0) {
         return;
     }
-    var navBarElem = $(".md-header nav");
+    var navBarElem = $('.md-header nav');
     navBarElem.append(
-        "<select id=\"releaseNavSelectBox\" name=\"releaseNav\" onchange=\"location = this.value;\">"
-      + " <option value=\"/latest/\">latest</option>"
-      + " <option value=\"/v0.12.1/\">v0.12.1</option>"
-      + "</select>");
+        '<select id="releaseNavSelectBox" name="releaseNav" onchange="location = this.value;">' +
+        ' <option value="/latest/">latest</option>' +
+        ' <option value="/v0.12.1/">v0.12.1</option>' +
+        '</select>');
 });
