cfde702f6e0af170c740ecd5988c18a1f58ec10b
Anthony Astolfi <tastolfi@gmail.com>
2022-10-26 15:04:53 -0400

    Fix javascript

--- releaseNav.js
+++ releaseNav.js
@@ -1,5 +1,11 @@
-document.onload(function () { if ($("#releaseNavSelectBox")) { return; } var navBarElem = $(".md-header nav");
-navBarElem.append("<select id=\"releaseNavSelectBox\" name=\"releaseNav\" onchange=\"location = this.value;\">"
- + "<option value=\"/latest/\">latest</option>"
-+ " <option value=\"/v0.12.1/\">v0.12.1</option>"
-+ "</select>");});
+window.addEventListener("load", function () {
+    if ($("#releaseNavSelectBox")) {
+        return;
+    }
+    var navBarElem = $(".md-header nav");
+    navBarElem.append(
+        "<select id=\"releaseNavSelectBox\" name=\"releaseNav\" onchange=\"location = this.value;\">"
+      + " <option value=\"/latest/\">latest</option>"
+      + " <option value=\"/v0.12.1/\">v0.12.1</option>"
+      + "</select>");
+});
