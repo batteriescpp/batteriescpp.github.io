4ff75722243227306ada54bbb4c963a703da0a50
Anthony Astolfi <tastolfi@gmail.com>
2022-10-26 16:08:51 -0400

    wip

--- releaseNav.js
+++ releaseNav.js
@@ -1,10 +1,19 @@
+function handleReleaseNav()
+{
+    console.log('RELEASE NAV ' + $('#releaseNavSelectBox').val());
+}
+
 window.addEventListener('load', function() {
     if ($('#releaseNavSelectBox').length !== 0) {
+        console.log('already ran releaseNav');
         return;
     }
     var navBarElem = $('.md-header nav');
+
+
+
     navBarElem.append(
-        '<select id="releaseNavSelectBox" name="releaseNav">' +
+        '<select id="releaseNavSelectBox" name="releaseNav" onchange="handleReleaseNav();">' +
         ' <option value="/latest">latest</option>' +
         ' <option value="/v0.12.1">v0.12.1</option>' +
         '</select>');
@@ -12,7 +21,7 @@ window.addEventListener('load', function() {
     var versionPattern = /^\/[^\/]*/;
     var actualVer = location.pathname.match(versionPattern)[0];
     $('#releaseNavSelectBox option[value="' + actualVer + '"]').prop('selected', 'selected');
-    $('#releaseNavSelectBox').on('change', function() {
+    $('#releaseNavSelectBox').on('select', function() {
         location.pathname.replace(versionPattern, $('#releaseNavSelectBox').val());
     });
 });
