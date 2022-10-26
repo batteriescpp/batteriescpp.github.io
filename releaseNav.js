8c4f275db4debf2b52e177e0137daefa108f0ee5
Anthony Astolfi <tastolfi@gmail.com>
2022-10-26 16:18:52 -0400

    wip

--- releaseNav.js
+++ releaseNav.js
@@ -3,6 +3,8 @@ function handleReleaseNav(event)
     console.log('RELEASE NAV ' + $('#releaseNavSelectBox').val());
 }
 
+var releaseNavPathPattern = /^\/[^\/]*/;
+
 window.addEventListener('load', function() {
     if ($('#releaseNavSelectBox').length !== 0) {
         console.log('already ran releaseNav');
@@ -10,16 +12,11 @@ window.addEventListener('load', function() {
     }
     var navBarElem = $('.md-header nav');
     navBarElem.append(
-        '<select id="releaseNavSelectBox" name="releaseNav">' +
+        '<select id="releaseNavSelectBox" name="releaseNav" onchange="location.pathname.replace(releaseNavPathPattern, this.value)">' +
         ' <option value="/latest">latest</option>' +
         ' <option value="/v0.12.1">v0.12.1</option>' +
         '</select>');
 
-    var versionPattern = /^\/[^\/]*/;
-    var actualVer = location.pathname.match(versionPattern)[0];
+    var actualVer = location.pathname.match(releaseNavPathPattern)[0];
     $('#releaseNavSelectBox option[value="' + actualVer + '"]').prop('selected', 'selected');
-    document.getElementById('releaseNavSelectBox').addEventListener('change', handleReleaseNav);
-    //$('#releaseNavSelectBox').on('select', function() {
-    //    location.pathname.replace(versionPattern, $('#releaseNavSelectBox').val());
-    //});
 });
